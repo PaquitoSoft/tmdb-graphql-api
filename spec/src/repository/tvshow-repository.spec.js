5e63ec6270f0f4b42977ae16740aee2f354d7542
Paquitosoft <paquitosoftware@gmail.com>
2019-11-17 18:24:14 +0100

    Added new test for TvShowRepository

--- spec/src/repository/tvshow-repository.spec.js
+++ spec/src/repository/tvshow-repository.spec.js
@@ -14,21 +14,41 @@ describe('TvShow Repository', () => {
 
 	describe('getByType', () => {
 
-		it('Should load popular tv shows', async () => {
-			const fixture = loadFixture('popular-tvshows.json');
+		async function testByType({
+			type,
+			fixtureName,
+			requestPath
+		}) {
+			const fixture = loadFixture(fixtureName);
 			const requestFake = sinon.fake.returns(fixture);
 			const tvShows = await getRepository(requestFake).getByType({
-				type: TvShowRepository.types.POPULAR,
+				type,
 				language: 'en'
 			});
 
-			expect(requestFake.lastArg).to.have.property('path', '/3/tv/popular');
+			expect(requestFake.lastArg).to.have.property('path', requestPath);
 			expect(tvShows).to.have.length(20);
 
 			const firstFixture = fixture.results[0];
 			const firstResult = tvShows[0];
 			expect(firstResult.name).to.equal(firstFixture.name);
 			expect(firstResult.votesCount).to.equal(firstFixture.vote_count);
+		}
+
+		it('Should load popular tv shows', async () => {
+			await testByType({
+				type: TvShowRepository.types.POPULAR,
+				fixtureName: 'popular-tvshows.json',
+				requestPath: `/3/tv/${TvShowRepository.types.POPULAR}`
+			});
+		});
+
+		it('Should load top rated tv shows', async () => {
+			await testByType({
+				type: TvShowRepository.types.TOP_RATED,
+				fixtureName: 'top_rated-tvshows.json',
+				requestPath: `/3/tv/${TvShowRepository.types.TOP_RATED}`
+			});
 		});
 
 	});
