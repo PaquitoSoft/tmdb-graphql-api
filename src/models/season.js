b4e847f14d59dddd31c44dc988753ebff4ecca6c
Paquitosoft <paquitosoftware@gmail.com>
2019-12-02 12:07:08 +0100

    Filter out special content ‘seasons’

--- src/models/season.js
+++ src/models/season.js
@@ -2,13 +2,21 @@ const Episode = require('./episode');
 
 class Season {
 	constructor(raw) {
+		const now = Date.now();
 		this.id = raw.id;
 		this.name = raw.name;
 		this.seasonNumber = raw.season_number;		
 		this.posterPath = raw.poster_path;
 		this.airDate = raw.air_date;
 		this.episodesCount = raw.episode_count;
-		this.episodes = (raw.episodes || []).map(epi => new Episode(epi));
+		/*
+			External API is returning episodes that are yet
+			to be aired but with no useful data, so I filter
+			them out.
+		*/
+		this.episodes = (raw.episodes || [])
+			.filter(epi => (new Date(epi.air_date)).getTime() < now)
+			.map(epi => new Episode(epi));
 	}
 }
 
